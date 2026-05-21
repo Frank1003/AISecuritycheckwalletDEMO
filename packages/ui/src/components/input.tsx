b91a28bc6cb0171063d13f29c0c5f1a7627c50a1
frank1003 <xxf10000@126.com>
2026-05-22 00:40:37 +0800

    deploy: publish dist to gh-pages

--- packages/ui/src/components/input.tsx
+++
@@ -1,6 +0,0 @@
-import type { InputHTMLAttributes } from 'react'
-import { cn } from '../utils'
-
-export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={cn('ui-input', props.className)} />
-}
--- packages/ui/src/components/select.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithChildren, SelectHTMLAttributes } from 'react'
-import { cn } from '../utils'
-
-export function Select(props: PropsWithChildren<SelectHTMLAttributes<HTMLSelectElement>>) {
-  return <select {...props} className={cn('ui-select', props.className)}>{props.children}</select>
-}
